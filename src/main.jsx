07a8b42e1075af6518bd0f9483ed1b07fb28f497
David <juanlc1526@gmail.com>
2023-12-05 18:06:33 -0500

    bad debugging

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
-import NavBar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
-   
-   <NavBar />
    <App />
    <Footer />
  </BrowserRouter>
